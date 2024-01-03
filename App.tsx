659c45274f7d6000a376cfb3efff64398938f277
Your Name <you@example.com>
2024-01-03 10:44:27 -0600

    Header completed

--- App.tsx
+++ App.tsx
@@ -18,7 +18,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "red",
+    backgroundColor: "#fff",
     alignItems: "center",
   },
   title: {
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,46 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
+import { Entypo } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const Header = () => {
+  const handlePress = () => {
+    Alert.alert("Button Pressed", "You pressed the icon button!");
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>1RM</Text>
+      <TouchableOpacity style={styles.leftContainer} onPress={handlePress}>
+        <Entypo name="menu" size={64} color="#777777" />
+      </TouchableOpacity>
+      <View style={styles.centerContainer}>
+        <Image style={styles.logo} source={require("../../assets/Logo.png")} />
+      </View>
+      <TouchableOpacity style={styles.rightContainer}>
+        <MaterialCommunityIcons
+          name="dots-vertical"
+          size={64}
+          color="#777777"
+        />
+      </TouchableOpacity>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "blue",
-    height: 80,
+    backgroundColor: "#0766AD",
+    height: 115, //115
     width: "100%",
+    justifyContent: "space-around",
+    flexDirection: "row",
     alignItems: "center",
   },
   title: {
@@ -21,5 +48,12 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 50,
   },
+  logo: {
+    width: 134,
+    height: 120,
+  },
+  leftContainer: {},
+  centerContainer: {},
+  rightContainer: {},
 });
 export default Header;
