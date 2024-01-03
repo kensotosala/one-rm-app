cdec14873aff287ccd0550ed3598fe694fd27d68
Your Name <you@example.com>
2024-01-03 11:57:41 -0600

    Form design completed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,8 +1,85 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { Input } from "@rneui/themed";
+import DropDownPicker from "react-native-dropdown-picker";
 
 const Form = () => {
-  return <View></View>;
+  // Dropdown values
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    { label: "Kg", value: "kg" },
+    { label: "lb", value: "lb" },
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculate your 1RM</Text>
+      <View style={styles.weightInput}>
+        <View style={styles.leftContainer}>
+          <Input ref={null} label="Weight" labelStyle={{ color: "#000" }} />
+        </View>
+        <View style={styles.rightContainer}>
+          <DropDownPicker
+            open={open}
+            value={value}
+            items={items}
+            setOpen={setOpen}
+            setValue={setValue}
+            setItems={setItems}
+            placeholder={"lb"}
+          />
+        </View>
+      </View>
+      <View>
+        <View>
+          <Input ref={null} label="Reps" labelStyle={{ color: "#000" }} />
+        </View>
+      </View>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity style={styles.calculateBtn}>
+          <Text style={{ color: "#fff", fontWeight: "bold", fontSize: 16 }}>
+            Calculate
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    width: "100%",
+    paddingHorizontal: 30,
+  },
+  title: {
+    textAlign: "center",
+    marginTop: 10,
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  label: {
+    fontSize: 16,
+  },
+  weightInput: {
+    marginTop: 25,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  leftContainer: {
+    width: "80%",
+  },
+  rightContainer: {
+    width: "20%",
+  },
+  btnContainer: {},
+  calculateBtn: {
+    backgroundColor: "#0766AD",
+    color: "#fff",
+    paddingVertical: 20,
+    alignItems: "center",
+    borderRadius: 20,
+  },
+});
 export default Form;
